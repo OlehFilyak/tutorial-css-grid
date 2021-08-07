566e38c54cde83c38569ea81c6b4235c4e9b8f17
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-08-07 15:30:27 +0300

    add conteiners and reset styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="box-1"> A </div>
+        <div className="box-2"> B </div>
+        <div className="box-3"> C </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
